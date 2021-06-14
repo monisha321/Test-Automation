0c865ebadef71bb39224945568a54997cbb9e4f5
monisha321 <monisha3331992@gmail.com>
2021-06-14 18:26:21 +0530

    new act

--- Web-Automation/src/com/sgteating/assgnDataProviderTestNG/ReflectionDemo1.java
+++ Web-Automation/src/com/sgteating/assgnDataProviderTestNG/ReflectionDemo1.java
@@ -8,7 +8,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-public class ReflectionDemo1 {
+public class ReflectionDemo1 {////test initiates here or entry point for tests
 
 
 	public static void main(String[] args) {
